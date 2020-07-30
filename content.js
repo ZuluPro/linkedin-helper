a9a2f972968de7db30e25cb01ff4dfb49b5d31f0
Anthony Monthe <anthony.monthe@gmail.com>
2020-07-30 14:47:35 +0100

    Addded link inclusion

--- content.js
+++ content.js
@@ -6,3 +6,27 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+function getCompanyId() {
+    return 38149327;
+}
+
+function addLink() {
+    [].forEach.call(document.querySelectorAll('[data-id^="urn:li:activity:"]'), function(elm){
+        if (elm.getAttribute("data-got-link") === 'true') return
+
+        console.log(elm);
+        var url = "/feed/update/" + elm.getAttribute("data-id") + '?actorCompanyId=' + getCompanyId();
+        var link = document.createElement("a");
+        link.setAttribute("href", url);
+        var text = document.createTextNode("Link");
+        link.appendChild(text);
+
+        var menu = elm.querySelector('.feed-shared-update-v2__control-menu');
+        if (menu === null) return
+
+        menu.append(link);
+        elm.setAttribute('data-got-link', 'true')
+    });
+}
+setInterval(addLink, 3000);
